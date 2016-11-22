f2deb9a36dc47e8d89e33b5831b6d4d90f7a5441
liuyanan <oceanwave@yeah.net>
2016-11-22 15:31:53 +0800

    添加了链表反转的例子

---
+++ algorithm_box/linklist_reverse.cpp
@@ -0,0 +1,102 @@
+/*
+ * linklist_reverse.cpp
+ *
+ *  Created on: Nov 22, 2016
+ *      Author: liuyanan
+ */
+
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct Node{
+	int value;
+	Node* p_next;
+};
+
+
+Node* construct_linklist(vector<int> values)
+{
+	Node* head  = NULL;
+	Node* tmp =  NULL;
+
+	for(auto value : values) {
+		Node* new_node = new Node;
+		new_node->value = value;
+		new_node->p_next = NULL;
+
+		// add to the list
+		if (tmp) {
+			tmp -> p_next = new_node;
+		} else {
+			head = new_node;
+		}
+
+		tmp = new_node;
+	}
+
+	return head;
+}
+
+Node* reverse(Node* head)
+{
+	if (!head) {
+		cerr << "empty linklist when reverse" << endl;
+	}
+
+	Node* p_reverse_head = NULL;
+	Node* pre = NULL;
+	Node* cur = head;
+	Node* next = NULL;
+
+	while(cur) {
+		 next = cur->p_next;		// 这个是保存 cur->p_next
+
+		if (next == NULL)
+			p_reverse_head = cur;
+
+		cur->p_next = pre;
+
+		pre = cur;
+		cur = next;
+	}
+
+	return p_reverse_head;
+}
+
+
+void output_linklist(Node* head) {
+
+	if ( !head ) {
+		cerr << "empty linklist" << endl;
+	}
+
+	Node *tmp = head;
+	while(tmp) {
+		cout << tmp->value << '\t';
+		tmp = tmp->p_next;
+	}
+
+	cout << endl;
+}
+
+
+int main()
+{
+
+	Node* list = NULL;
+
+	list = construct_linklist({1, 2, 3, 4, 5, 6, 7, 8});
+
+	output_linklist(list);
+
+	list = reverse(list);
+
+	output_linklist(list);
+
+	return 0;
+}
