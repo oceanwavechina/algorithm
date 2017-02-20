50bfdfaedfb8dd541bb2ade4eb5ee7a94d879370
liuyanan <oceanwave@yeah.net>
2017-02-20 18:19:03 +0800

    添加了上台阶问题和计算树的深度

---
+++ algorithm_box/tree_depth.cpp
@@ -0,0 +1,87 @@
+/*
+ * tree_depth.cpp
+ *
+ *  Created on: Feb 20, 2017
+ *      Author: liuyanan
+ */
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <stack>
+
+
+using namespace std;
+
+struct Node
+{
+	Node(int v):value(v), left(NULL), right(NULL) {}
+	~Node() {}
+
+	int value;
+	Node* left;
+	Node* right;
+};
+
+
+Node* createNode(int value)
+{
+	return new Node(value);
+}
+
+void buildTree(Node* parent, Node* left, Node* right)
+{
+	if (parent != NULL) {
+		parent->left = left;
+		parent->right = right;
+	}
+}
+
+Node* getTestTree() {
+
+	/*
+	 *
+	 *			 10
+	 *			/  \
+	 *		   6    14
+	 *		  / \   / \
+	 *		 4   8 12 16
+	 *		/
+	 *	   9
+	 *
+	 */
+
+	Node* node10 = createNode(10);
+	Node* node6 = createNode(6);
+	Node* node14 = createNode(14);
+	Node* node4 = createNode(4);
+	Node* node8 = createNode(8);
+	Node* node12 = createNode(12);
+	Node* node16 = createNode(16);
+	Node* node9 = createNode(4);
+
+	buildTree(node10, node6, node14);
+	buildTree(node6, node4, node8);
+	buildTree(node14, node12, node16);
+
+	return node10;
+}
+
+int depth(Node* p) {
+
+	if (!p)
+		return 0;
+
+	return max(depth(p->left), depth(p->right)) + 1;
+}
+
+int main(int argc, char **argv) {
+
+	cout << "tree depth:" << depth(getTestTree()) << endl;
+
+	return 0;
+}
+
+
+
+
---
+++ algorithm_box/up_the_steps.cpp
@@ -0,0 +1,50 @@
+/*
+ * up_the_steps.cpp
+ *
+ *  Created on: Feb 17, 2017
+ *      Author: liuyanan
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+/*
+ * 楼梯有n阶台阶，上楼可以一步上1阶，也可以一步上2阶, 有多少种不同的走法
+ *
+ * 问题解析:
+ * 		我们现在想象自己已经站在第n级台阶上了，那么我们上一个位置只能在第n-1或者n-2级台阶上。
+ * 		比如我们在第3级台阶上，我们上一个位置就在第1或者第2级台阶上。
+ * 		也就是说我们到达第3级台阶有两种情况，分别计算着两种情况并相加即可，即到达第1级台阶的方式数加上到达第2级台阶的方式数，结果等于3。
+ * 		同理到达第n级台阶的放法数就等于到达第n-1级台阶与到达第n-2级台阶数之和。这就是一个递归的过程（自己调用自己只是传入参数不一样），递推方程为：
+ *
+ *				|-> 1, 					n=1
+ *		F(n)  = |-> 2,					n=2
+ *				|-> F(n-1) + F(n-2), 	n>2
+ */
+int recursive_count(int n) {
+
+	if(n < 0) {
+		return 0;
+	}
+
+	cout << n << ' ';
+	if (n <= 2) {
+		cout << endl;
+		return n;
+	}
+
+	return recursive_count(n-1) + recursive_count(n-2);
+}
+
+
+int main(int argc, char **argv) {
+
+	recursive_count(7);
+//	cout << recursive(7) << endl;
+
+	return 0;
+}
