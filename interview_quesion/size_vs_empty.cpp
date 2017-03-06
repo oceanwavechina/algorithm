0f1db4f092934b479ad49a1cef7be386d18488b8
liuyanan <oceanwave@yeah.net>
2017-03-06 17:09:12 +0800

    添加了对比不同版本c++下 list对象的大小

--- interview_quesion/size_vs_empty.cpp
+++ interview_quesion/size_vs_empty.cpp
@@ -13,10 +13,12 @@
  * http://stackoverflow.com/questions/10065055/why-is-stdlist-bigger-on-c11
  *
  * 在c++98下，安装老版本g++的机器：
+ * 		sizeof(list): 16
  * 		size()==0 : 0m0.536s
  * 		empty()	  : 0m0.090s
  *
  * 在c++11下:
+ *		sizeof(list): 24
  *		size()==0 : 0m0.153s
  * 		empty()	  : 0m0.148s
  */
@@ -33,6 +35,7 @@ using namespace std;
 int main(int argc, char **argv) {
 
 	list<int> a;
+	cout <<  "sizeof(list): " << sizeof(a) << endl;
 	for(int i=0; i<1000000; ++i) {
 		a.push_back(i);
 	}
