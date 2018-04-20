c38b1e7d53d9df9572f3ea856f3b49714099b213
liuyanan <oceanwave@yeah.net>
2018-04-20 20:53:36 +0800

    cpp tricks

--- cpp/tricks.cpp
+++ cpp/tricks.cpp
@@ -6,22 +6,52 @@
  */
 
 #include <iostream>
-#include <list>
+#include <stdexcept>
+#include <cassert>
 
+/*
+ * try 初始化列表，还可以这样用
+ * https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Construction_Tracker
+ */
 
-using namespace std;
+struct B {
+    B (char const *) { throw std::runtime_error("B Error"); }
+};
+struct C {
+    C (char const *) { throw std::runtime_error("C Error"); }
+};
+class A {
+   B b_;
+   C c_;
+   enum TrackerType { NONE, ONE, TWO };
+public:
+   A( TrackerType tracker = NONE)
+   try    // A constructor try block.
+     : b_((tracker = ONE, "hello")) // Can throw std::exception
+     , c_((tracker = TWO, "world")) // Can throw std::exception
+     {
+        assert(tracker == TWO);
+        // ... constructor body ...
+     }
+   catch (std::exception const & e)
+     {
+        if (tracker == ONE) {
+           std::cout << "B threw: " << e.what() << std::endl;
+        }
+        else if (tracker == TWO) {
+           std::cout << "C threw: " << e.what() << std::endl;
+        }
+        throw;
+     }
+};
 
-
-int main(int argc, char **argv) {
-
-
-	list<int> x;
-	x.push_back(10);
-	x.push_back(20);
-
-	for (auto i : x) {
-		cout << i;
-	}
-
-	return 0;
+int main (void)
+{
+    try {
+        A a;
+    }
+    catch (std::exception const & e) {
+          std::cout << "Caught: " << e.what() << std::endl;
+    }
+    return 0;
 }
