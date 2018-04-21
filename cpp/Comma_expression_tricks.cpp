315d30445439da141ea63115d2f87b088fa70b1c
liuyanan <oceanwave@yeah.net>
2018-04-21 12:02:30 +0800

    cpp tricks

--- cpp/Comma_expression_tricks.cpp
+++ cpp/Comma_expression_tricks.cpp
@@ -14,8 +14,15 @@
  * try 初始化列表，还可以这样用
  * https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Construction_Tracker
  *
- * https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms
+ * 这里边的技巧就是：
+ * 	1. 逗号表达式只返回最后一个表达式的结果
+ *
+ * 	In a comma expression E1, E2, the expression E1 is evaluated,
+ * 	its result is discarded (although if it has class type, it won't be destroyed until the end of the containing full expression),
+ * 	and its side effects are completed before evaluation of the expression E2 begins
  */
+
+using namespace std;
 
 struct B {
     B (char const *) { throw std::runtime_error("B Error"); }
@@ -51,6 +58,12 @@ public:
 int main (void)
 {
     try {
+    	int tracker(0);
+
+    	string str = (tracker = 1, "hello");
+    	// string str = ("hello", tracker = 1);	// error
+    	std::cout << "str: " << str << std::endl;
+    	std::cout << "tracker:" << tracker << endl;
         A a;
     }
     catch (std::exception const & e) {
